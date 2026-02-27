f19a879f95c3831f14f77b55e417912e1f1e07f7
Noorja <siddhaigarunoorjahan@gmail.com>
2026-02-27 20:17:22 +0530

    modules

--- modules/codedeploy/outputs.tf
+++ modules/codedeploy/outputs.tf
@@ -1,14 +1,11 @@
 output "codedeploy_app_name" {
-  description = "CodeDeploy application name"
-  value       = aws_codedeploy_app.this.name
+  value = aws_codedeploy_app.strapi_app.name
 }
 
 output "codedeploy_app_id" {
-  description = "CodeDeploy application ID"
-  value       = aws_codedeploy_app.this.id
+  value = aws_codedeploy_app.strapi_app.id
 }
 
 output "deployment_group_name" {
-  description = "CodeDeploy deployment group name"
-  value       = aws_codedeploy_deployment_group.this.deployment_group_name
+  value = aws_codedeploy_deployment_group.strapi_deployment_group.deployment_group_name
 }
